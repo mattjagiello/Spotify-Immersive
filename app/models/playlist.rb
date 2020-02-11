a4cf44d9179ddd0359a80913679fbe93e7f93035
github username <github email address>
2020-02-11 11:54:31 -0600

    set up environment for more changes

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,4 +1,22 @@
 class Playlist < ActiveRecord::Base
     belongs_to :user
     has_and_belongs_to_many :songs
+
+    def add_song(title, artist)
+        song = RSpotify::Track.search(title)
+
+        track_found = song.select do |track|
+            if track.album.artists[0].name == artist
+                track
+            end
+        end
+
+        uri_found = track_found[0].album.artists[0].uri
+
+        s = Song.create(title: title, uri: uri_found)
+        a = Artist.create(name: title)
+        SongsArtists.create(song: s, artist: a)
+        PlaylistsSongs.create(playlist: self, song: s)
+    end
+
 end
--- db/migrate/20200210183007_create_songs.rb
+++ db/migrate/20200210183007_create_songs.rb
@@ -2,6 +2,7 @@ class CreateSongs < ActiveRecord::Migration[5.0]
   def change
     create_table :songs do |t|
       t.string :title
+      t.string :uri
     end
   end
 end
