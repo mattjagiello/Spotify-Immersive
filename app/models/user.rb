2cfe0d7abcfdd9f316562e6242f0457741ad54fd
mattjagiello <mjagiello@gmail.com>
2020-02-12 14:45:11 -0600

    Modified user methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,29 +5,30 @@ class User < ActiveRecord::Base
 #Prompt for user login/password, calls self.find_or_create_user method
 def self.login
 prompt = TTY::Prompt.new
-name = prompt.ask ('Hello! Enter username to login or create new user.') do |q|
+name = prompt.ask ("Enter username to login or create new user. Type 'Exit' to quit.") do |q|
     q.required true
     q.validate /\A\w+\Z/
     end
+    if name == "Exit"
+        exit
+    end
 password = prompt.mask("Enter a password - not required.")
 User.find_or_create_user(name, password)
-User.view_playlists
+puts "#{name}'s Playlist Options"
+login_user = User.all.select{|x| x.name == name}
+login_user[0].playlist_options
 end
 
 #TTY prompt to get argument, return user if found or create user if not found
-#Need to add password functionality via run console
     def self.find_or_create_user(username, password = nil)
         userlist = User.all.collect {|x| x.name}
         if userlist.include?(username)
             uobject = User.all.select{|x| x.name == username}
                 if uobject[0].password != password
-                    p "Wrong password, re-run program! :("
-                    #username = gets.chomp
-                    #password = gets.chomp
-                    #find_or_create_user(username, password)
-                    return 0
+                    p "Wrong password, try again!"
+                    User.login
                 end
-            @uid = uobject[0].id
+            id = uobject[0].id
             uname = uobject[0].name
             p "Welcome back, #{uname}!"
         else
@@ -37,11 +38,53 @@ end
         end
     end
 
+#View Playlist Options
+    def playlist_options
+        prompt = TTY::Prompt.new
+        selection = prompt.select("Choose an option.") do |menu|
+            menu.choice name: 'View My Playlists', value: 1
+            menu.choice name: 'Add New Playlist', value: 2
+            menu.choice name: 'Delete A Playlist', value: 3
+            menu.choice name: 'Logout', value: 4
+          end
+    
+        case selection
+        when 1
+            self.view_playlists
+        when 2
+            plname = prompt.ask ('Enter a playlist name.') do |q|
+                q.required true
+            end
+            self.generate_playlist(plname)
+            p "Playlist #{plname} created!"
+            self.playlist_options
+        when 3
+            name = prompt.ask ("Enter a playlist name to delete or type 'Back' to go back.") do |q|
+                q.required true
+                if name == "Back"
+                    self.playlist_options
+                end
+            end
+            all_plnames = Playlist.all.collect{|x| x.name}
+            if all_plnames.exclude?(name)
+                puts "View playlists and try again." 
+            else
+                self.remove_playlist(name)
+                puts "Playlist #{name} deleted!"
+            end
+            self.playlist_options
+        when 4
+            puts "Bye!"
+            User.login
+        end
+    end
+
 #View Playlists
-    def self.view_playlists
-        userplaylists = Playlist.where(user_id: @uid)
+    def view_playlists
+        userplaylists = Playlist.where(user_id: self.id)
         if userplaylists.empty?
-            print "No playlists found."
+            print "No playlists found. "
+            self.playlist_options
         else plnames = userplaylists.collect {|x| x.name} 
             plnames.map.with_index(1) do |playlist, id|
             puts "#{id} - #{playlist}"
@@ -50,8 +93,8 @@ end
     end
     
 #Generate Playlist - TTY prompt for name
-    def self.generate_playlist(name)
-        Playlist.create(name: name, user_id: @uid)
+    def generate_playlist(name)
+        Playlist.create(name: name, user_id: self.id)
     end
 
 #Generate Playlist by Genre - TTY prompt for name, genre, number
@@ -59,7 +102,7 @@ end
         RSpotify.authenticate("a09377aa120c4a68ba377892982cb5cf", "c3a52e31188c43b6930c737fbe8a3026")
         generate_playlist(name)
         lastpl = Playlist.last
-        lastpl.user_id = @uid
+        lastpl.user_id = self.id
         if number > 10
             number = 10
         end
@@ -77,7 +120,7 @@ end
     end
 
 #Remove Playlist
-    def self.remove_playlist(name)
+    def remove_playlist(name)
         if Playlist.where(name: name) == []
             p "No matching playlist."
         else
@@ -86,5 +129,3 @@ end
     end
 
 end
-
-
